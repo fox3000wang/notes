397b6164be8065abe808dd103ebd987751f7ea1b
fox.wang <fox3000wang@hotmail.com>
2024-03-26 13:37:05 +0800

    爱奇艺脚本完成

--- 92-安卓自动化测试/挂机脚本/main.py
+++ 92-安卓自动化测试/挂机脚本/main.py
@@ -7,27 +7,49 @@ d.info
 # 得出设备链接信息
 print(d.window_size())
 # 获取屏幕大小
-print(d.current_app())
-# 获取当前应用的信息
-print(d.serial)
-# 获取设备序列号
-print(d.wlan_ip)
-# 获取WIFI IP
-print(d.device_info)
-# 获取详细的设备信息
+
+# 关闭所有app 千万不要用啊，会杀掉后台进程
+# d.app_stop_all()
 
 
-#
-package_name = 'com.taobao.taobao'
+def taobao():
+    # 获取包名
+    package_name = 'com.taobao.taobao'
 
-# 打开淘宝app
-d.app_start(package_name)
+    # 打开淘宝app
+    d.app_start(package_name)
 
-# 等待10秒钟 A10环境下经验值
-time.sleep(10)
+    # 等待10秒钟 A10环境下经验值
+    time.sleep(10)
 
-# 点击坐标100，100
-# d.click(100, 100)
+    # 点击坐标100，100
+    d.click(106, 106)
 
-# 关闭淘宝
-d.app_stop(package_name)
+    time.sleep(10)
+
+    # 关闭淘宝
+    # d.app_stop(package_name)
+
+
+def qiyi():
+    package_name = 'com.qiyi.video.lite'
+    d.app_start(package_name)
+    time.sleep(10)
+
+    # 点击赚钱
+    d.click(363, 1519)
+    time.sleep(10)
+
+    # 点击提现
+    d.click(610, 180)
+    time.sleep(5)
+
+    # 点击微信0.3元提现
+    d.click(140, 500)
+    time.sleep(5)
+
+    # 关闭
+    d.app_stop(package_name)
+
+
+qiyi()
