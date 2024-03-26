82453c430cdc050fbced0265be54c94a517fe9a5
fox.wang <fox3000wang@hotmail.com>
2024-03-26 12:47:54 +0800

    update

--- 92-安卓自动化测试/hello_ uiautomator2.py
+++ 92-安卓自动化测试/hello_ uiautomator2.py
@@ -2,3 +2,5 @@ import uiautomator2 as u2
 
 d = u2.connect('192.168.0.106')  # alias for u2.connect_wifi('10.0.0.1')
 print(d.info)
+
+# d.app_start('com.tencent.mm')
--- 92-安卓自动化测试/挂机脚本/main.py
+++ 92-安卓自动化测试/挂机脚本/main.py
@@ -1,21 +1,33 @@
 import uiautomator2 as u2
 import time
 
-device = u2.connect('192.168.0.106')
+d = u2.connect('192.168.0.109')
+
+d.info
+# 得出设备链接信息
+print(d.window_size())
+# 获取屏幕大小
+print(d.current_app())
+# 获取当前应用的信息
+print(d.serial)
+# 获取设备序列号
+print(d.wlan_ip)
+# 获取WIFI IP
+print(d.device_info)
+# 获取详细的设备信息
+
+
+#
+package_name = 'com.taobao.taobao'
 
 # 打开淘宝app
-# d.app_start('com.taobao.taobao')
-
-# 打开抖音app
-app = device.app_start("com.ss.android.ugc.aweme.lite")
-
+d.app_start(package_name)
 
 # 等待10秒钟 A10环境下经验值
 time.sleep(10)
 
-
 # 点击坐标100，100
-app.click(100, 100)
+# d.click(100, 100)
 
-# 关闭淘宝app
-app.app_stop('com.ss.android.ugc.aweme.lite')
+# 关闭淘宝
+d.app_stop(package_name)
